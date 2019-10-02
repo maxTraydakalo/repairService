2bb5a8d3f8b0cd17b5071fe3c409e6fc7643bebf
max.traydakalo@gmail.com <nhfqlfrkfj>
2019-10-02 11:08:43 +0300

    master facilities and refactored manager pagination

--- src/main/java/com/traydakalo/aLearn/main.java
+++
@@ -1,9 +0,0 @@
-package com.traydakalo.aLearn;
-
-import com.traydakalo.security.SecurityConfig;
-
-public class main {
-    public static void main(String[] args) throws Exception {
-
-    }
-}
--- src/main/java/com/traydakalo/dao/queries/UserDaoInterface.java
+++ src/main/java/com/traydakalo/dao/queries/UserDaoInterface.java
@@ -10,28 +10,10 @@ import java.util.List;
 public interface UserDaoInterface {
     // Actions ------------------------------------------------------------------------------------
 
-    /**
-     * Returns the user from the database matching the given ID, otherwise null.
-     * @param id The ID of the user to be returned.
-     * @return The user from the database matching the given ID, otherwise null.
-     * @throws DaoException If something fails at database level.
-     */
      UserDto find(Long id) throws DaoException;
 
-    /**
-     * Returns the user from the database matching the given email and password, otherwise null.
-     * @param email The email of the user to be returned.
-     * @return The user from the database matching the given email and password, otherwise null.
-     * @throws DaoException If something fails at database level.
-     */
      User find(String email) throws DaoException;
 
-    /**
-     * Returns a list of all users from the database ordered by user ID. The list is never null and
-     * is empty when the database does not contain any user.
-     * @return A list of all users from the database ordered by user ID.
-     * @throws DaoException If something fails at database level.
-     */
      List<User> list() throws DaoException;
 
      void register(String login, String password);
